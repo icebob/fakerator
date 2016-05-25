4c6d21687a40a1982088dcae167627f63dbc4e47
Icebob <mereg.norbert@gmail.com>
2016-05-25 10:09:30 +0200

    Add uuid generator

---
+++ test/specs/locales/default/misc.spec.js
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+
+import Fakerator from "lib/fakerator";
+
+describe("Default misc", () => {
+
+	let fakerator;
+
+	beforeEach( () => {
+		fakerator = new Fakerator();
+		fakerator.seed(8080);
+	});
+
+	it("check misc.uuid", () => {
+		expect(fakerator.populate("#{misc.uuid}")).to.be
+			.an("String")
+			.length(36)
+			.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/);
+
+		expect(fakerator.misc.uuid()).to.be
+			.an("String")
+			.length(36)
+			.match(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/);
+	});
+
+})
